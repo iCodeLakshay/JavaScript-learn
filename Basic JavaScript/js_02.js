6b67c66327fd996c71887de3652326e37d6cf079
Lakshay Saxena <163431247+iCodeLakshay@users.noreply.github.com>
2024-10-21 13:29:52 +0000

    code runner

---
+++ Basic JavaScript/js_02.js
@@ -0,0 +1,13 @@
+let number = null
+console.log(typeof number);
+
+let number1 = undefined
+console.log(typeof number1);
+
+let realNum = Number(number1)
+console.log(realNum  + " " + typeof realNum);
+
+let realNum1 = Number(number)
+console.log(realNum1  + " " + typeof number);
+
+
